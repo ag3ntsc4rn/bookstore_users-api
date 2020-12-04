fbfbafe5935e763ad2a37b6873589001e6e255b0
Manish Menon <19154062+ag3ntsc4rn@users.noreply.github.com>
2020-12-04 13:52:53 -0500

    MySQL Utils

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -2,18 +2,16 @@ package users
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/ag3ntsc4rn/bookstore_users-api/datasources/mysql/usersdb"
 	"github.com/ag3ntsc4rn/bookstore_users-api/utils/date"
 	"github.com/ag3ntsc4rn/bookstore_users-api/utils/errors"
+	mysqlutils "github.com/ag3ntsc4rn/bookstore_users-api/utils/mysql"
 )
 
 const (
-	indexUniqueEmail = "email_UNIQUE"
-	noRowsInResultSet = "no rows in result set"
-	queryInsertUser  = "INSERT INTO users(first_name, last_name, email, date_created) VALUES (?,?,?,?);"
-	queryGetUser     = "SELECT id, first_name, last_name, email, date_created FROM users WHERE id=?;"
+	queryInsertUser   = "INSERT INTO users(first_name, last_name, email, date_created) VALUES (?,?,?,?);"
+	queryGetUser      = "SELECT id, first_name, last_name, email, date_created FROM users WHERE id=?;"
 )
 
 var (
@@ -29,11 +27,7 @@ func (user *User) Get() *errors.RestErr {
 	defer stmt.Close()
 	result := stmt.QueryRow(user.ID)
 	if err := result.Scan(&user.ID, &user.FirstName, &user.LastName, &user.Email, &user.DateCreated); err != nil {
-		if strings.Contains(err.Error(), noRowsInResultSet) {
-			return errors.NewNotFoundError(fmt.Sprintf("no user found with id %v", user.ID))
-		}
-		return errors.NewInternalServerError(
-			fmt.Sprintf("error occured while retreiving result from database for user id %v:%v", user.ID, err.Error()))
+		return mysqlutils.ParseError(err)
 	}
 	return nil
 }
@@ -48,16 +42,11 @@ func (user *User) Save() *errors.RestErr {
 	user.DateCreated = date.GetNowString()
 	insertResult, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
 	if err != nil {
-		if strings.Contains(err.Error(), indexUniqueEmail) {
-			return errors.NewBadRequestError(
-				fmt.Sprintf("email %v already exists", user.Email))
-		}
-		return errors.NewInternalServerError(
-			fmt.Sprintf("error occured while inserting to database:%v", err.Error()))
+		return mysqlutils.ParseError(err)
 	}
 	userID, err := insertResult.LastInsertId()
 	if err != nil {
-		return errors.NewInternalServerError("error when trying to get last inserted id")
+		return mysqlutils.ParseError(err)
 	}
 	user.ID = userID
 	return nil
